446681e299d0083be873b10b0b9583e268d27cf2
Ken Javier <64310834+kcjavier21@users.noreply.github.com>
2020-10-04 09:08:50 +0800

    All done

--- js/post.js
+++ js/post.js
@@ -11,11 +11,36 @@ window.onload = () => {
     getPost();
 }
 
-const getPost = () => {
-    // CODE GOES HERE
+const getPostIdParam = () => {
+    const queryString = window.location.search;
+    const urlParams = new URLSearchParams(queryString);
+    return urlParams.get('id');
 }
+
+const getPost = () => {
+    const postId = getPostIdParam();
+    const url = `${API_URL}${postId}`;
+    
+    fetch(url, {
+        method: 'GET'
+    }).then((response) => {
+        return response.json();
+    }).then((data) => {
+        buildPost(data);
+    })
+}
+
 
 const buildPost = (data) => {
     // HINT: Convert the date number to a Date string 
+    const individualDate = new Date(parseInt(data.added_date)).toDateString();
+    const postImage = `${API_BASE_URL}${data.post_image}`;
+
+
+    document.querySelector('header').style.backgroundImage = `url(${postImage})`;
+    document.getElementById('individual-post-title').innerText = data.title;
+    document.getElementById('individual-post-date').innerText = `Published on ${individualDate}`;
+    document.getElementById('individual-post-content').innerText = data.content;
+    
 }
 
